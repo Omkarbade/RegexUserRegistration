24a81ef9b58bf9fe02f3556055f346611716ca36
Omkarbade <omakrbade3799@gmail.com>
2022-02-04 14:45:27 +0530

    [Omkar] Add Valid First Name program

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistration
+{
+    internal class Program
+    {
+        // <summary>
+        // Validating Password Rule 4
+        // </summary>
+        /// <param name="args">The args.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program\n");
+            UserRegistration userRegistration = new UserRegistration();      // Declaring object
+            Console.WriteLine("Enter valid first name");
+            string firstname = Console.ReadLine();
+            userRegistration.ValidateFName(firstname);
+
+        }
+    }
+}
---
+++ RegexUserRegistration/UserRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistration
+{
+    public class UserRegistration
+    { 
+        //Pattern for first name
+        public const string FIRST_NAME = "^[A-Z][a-zA-Z]{2}";
+
+        public void ValidateFName(string fName)
+        {
+            //using regex constructor and assigning pattern.
+            Regex regex = new Regex(FIRST_NAME);
+
+            if (regex.IsMatch(fName))
+            {
+                Console.WriteLine("first name is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("first name does not match with pattern");
+            }
+        }
+    }
+}
