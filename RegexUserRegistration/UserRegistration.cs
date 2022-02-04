95d4aa4acc3fa29f0aca59a80cc778714292761f
Omkarbade <omakrbade3799@gmail.com>
2022-02-04 15:03:47 +0530

    [Omkar] Add Valid Email program

---
+++ RegexUserRegistration/UserRegistration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistration
+{
+    public class UserRegistration
+    {
+        //Declaring Constant Variables for Patterns
+        public const string FIRST_NAME = "^[A-Z][a-zA-Z]{2}";
+        public const string LAST_NAME = "^[A-Z][a-zA-Z]{2}";
+        public const string EMAIL = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
+
+        public void ValidateFName(string fName)
+        {
+            //using regex constructor and assigning pattern.
+            Regex regex = new Regex(FIRST_NAME);
+
+            if (regex.IsMatch(fName))
+            {
+                Console.WriteLine("first name is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("first name does not match with pattern");
+            }
+        }
+        public void ValidateLName(string lName)
+        {
+            //using regex constructor and assigning pattern
+            Regex regex = new Regex(LAST_NAME);
+
+            if (regex.IsMatch(lName))
+            {
+                Console.WriteLine("Last name is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("Last name does not match with pattern");
+            }
+        }
+        public void ValidateEmail(string emailId)
+        {
+            ///using regex constructor and assigning pattern
+            Regex regex = new Regex(EMAIL);
+
+            if (regex.IsMatch(emailId))
+            {
+                Console.WriteLine("Email is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("Email not matched with pattern");
+            }
+        }
+    
+    }
+}
+
