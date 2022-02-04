1689a0088d215190c4c00024920226bec15ca78d
Omkarbade <omakrbade3799@gmail.com>
2022-02-04 15:24:11 +0530

    [Omkar] Add Valid Password Rule3 program

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistration
+{
+    public class Program
+    {
+        /// <summary>
+        /// Validating first name starting with cap letter and have atlest 3 characters 
+        /// </summary>
+        /// <param name="args">The args.</param>
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Welcome to User Registration Program\n");
+                UserRegistration userRegistration = new UserRegistration(); // Declaring object
+                //Console.WriteLine("Enter valid first name");
+                //string firstname = Console.ReadLine();
+                //userRegistration.ValidateFName(firstname);
+                //Console.WriteLine("Enter last name");
+                //string lastname = Console.ReadLine();
+                //userRegistration.ValidateLName(lastname);
+                //Console.WriteLine("Enter Email id");
+                //string email = Console.ReadLine();
+                //userRegistration.ValidateEmail(email);
+                //Console.WriteLine("Enter Mobile no");
+                //string monumber = Console.ReadLine();
+                //userRegistration.ValidateMobileNumber(monumber);
+                //Console.WriteLine("Enter Password");
+                //string password1 = Console.ReadLine();
+                //userRegistration.ValidatePasswordRule1(password1);
+                // Console.WriteLine("Enter Password");
+                //string password1 = Console.ReadLine();
+                //userRegistration.ValidatePasswordRule1(password1);
+                //Console.WriteLine("Enter password with atleast one upper case letter");
+                //string password2 = Console.ReadLine();
+                //userRegistration.ValidatePasswordRule2(password2);
+                Console.WriteLine("Enter password with atleast one upper case letter and one number");
+                string password3 = Console.ReadLine();
+                userRegistration.ValidatePasswordRule3(password3);
+            }
+        }
+            
+    }
+}
---
+++ RegexUserRegistration/UserRegistration.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistration
+{
+    public class UserRegistration
+    {
+        //Declaring Constant Variables for Patterns
+        public const string FIRST_NAME = "^[A-Z][a-zA-Z]{2}";
+        public const string LAST_NAME = "^[A-Z][a-zA-Z]{2}";
+        public const string EMAIL = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
+        public const string MOBILE_NO = "^([9][1])+[ ]+[6789]{1}[0-9]{9}$";
+        public const string PASSWORD_RULE1 = "^[a-z]{8}";
+        public const string PASSWORD_RULE2 = "^(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+        public const string PASSWORD_RULE3 = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+
+        //Method to check First Name
+        public void ValidateFName(string fName)
+        {
+            //using regex constructor and assigning pattern.
+            Regex regex = new Regex(FIRST_NAME);
+
+            if (regex.IsMatch(fName))
+            {
+                Console.WriteLine("first name is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("first name does not match with pattern");
+            }
+        }
+        //Method to check Last Name
+        public void ValidateLName(string lName)
+        {
+            //using regex constructor and assigning pattern
+            Regex regex = new Regex(LAST_NAME);
+
+            if (regex.IsMatch(lName))
+            {
+                Console.WriteLine("Last name is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("Last name does not match with pattern");
+            }
+        }
+        //Method to check Email id
+        public void ValidateEmail(string emailId)
+        {
+            ///using regex constructor and assigning pattern
+            Regex regex = new Regex(EMAIL);
+
+            if (regex.IsMatch(emailId))
+            {
+                Console.WriteLine("Email is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("Email not matched with pattern");
+            }
+        }
+        //Method to check Mobile number
+        public void ValidateMobileNumber(string monum)
+        {
+            //using regex constructor and assigning pattern
+            Regex regex = new Regex(MOBILE_NO);
+
+            if (regex.IsMatch(monum))
+            {
+                Console.WriteLine("Mobile Number is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("Mobile Number not matched with pattern");
+            }
+        }
+        //method to check password
+        public void ValidatePasswordRule1(string password1)
+        {
+            //using regex constructor and assigning pattern
+            Regex regex = new Regex(PASSWORD_RULE1);
+
+            if (regex.IsMatch(password1))
+            {
+                Console.WriteLine("PassWord is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("PassWord not matched with pattern");
+            }
+        }
+        //method to check passwrd 
+        public void ValidatePasswordRule2(string password2)
+        {
+            //using regex constructor and assigning pattern
+            Regex regex = new Regex(PASSWORD_RULE2);
+
+            if (regex.IsMatch(password2))
+            {
+                Console.WriteLine("PassWord is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("PassWord not matched with pattern");
+            }
+        }
+        //method to check passwrd 
+        public void ValidatePasswordRule3(string password3)
+        {
+            //using regex constructor and assigning pattern
+            Regex regex = new Regex(PASSWORD_RULE3);
+
+            if (regex.IsMatch(password3))
+            {
+                Console.WriteLine("PassWord is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("PassWord not matched with pattern");
+            }
+        }
+    
+    }
+}
+
